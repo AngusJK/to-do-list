4e00084d50f6e420cfb84ad35f6e5f5ecbb70014
Angus Kellett <anguskellett@hotmail.com>
2021-05-26 23:36:52 +0000

    adds header To Do List

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 
+import Form from './Components/Form';
+
+
 function App() {
   return (
     <div className="App">
-      <h1>Goodbye, React!</h1>
+      <header>
+        <h1>To Do List</h1>
+      </header>
+      <Form />
     </div>
   );
 }
