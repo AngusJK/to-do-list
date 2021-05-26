5c2d70ba18427d10e5ff5c67dd667ed975c5d5e6
Angus Kellett <anguskellett@hotmail.com>
2021-05-26 23:55:43 +0000

    imports Form and ToDoList

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 
 import Form from './Components/Form';
-
+import ToDoList from './Components/ToDoList';
 
 function App() {
   return (
     <div className="App">
       <header>
-        <h1>To Do List</h1>
+        <h1>Stephan's To Do List</h1>
       </header>
       <Form />
+      <ToDoList />
     </div>
   );
 }
