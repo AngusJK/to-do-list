c0505e239b9913261496d7dea773abf03bc1837f
Angus Kellett <anguskellett@hotmail.com>
2021-05-28 17:46:36 +0000

    passes todo, todos, and setTodos to each ToDo

--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ToDo from './ToDo'
 
-const ToDoList = ({ todos }) => {
+const ToDoList = ({ todos, setTodos }) => {
   return (
     <div className="todo-container">
       <ul className="todo-list">
         {todos.map((todo) => (
-          <ToDo key={todo.id} text={todo.text}/>
+          <ToDo 
+            key={todo.id} 
+            text={todo.text}
+            todo={todo}
+            todos={todos}
+            setTodos={setTodos}
+          />
         ))}
       </ul>
     </div>
