9b6953bf205bde7c6f009411d3fa0f7261009f87
Angus Kellett <anguskellett@hotmail.com>
2021-05-28 17:59:55 +0000

    implements completeHandler and calls it on complete button

--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -5,10 +5,18 @@ const ToDo = ({ text, todo, todos, setTodos }) => {
   const deleteHandler = () => {
     setTodos(todos.filter((el) => el.id !== todo.id))
   }
+  const completeHandler = () => {
+    setTodos(todos.map((el) => {
+      if(el.id === todo.id) {
+        return { ...el, completed: !el.completed }
+      }
+      return el;
+    }))
+  }
   return (
     <div className="todo">
       <li className="todo-item">{ text }</li>
-      <button className="complete-btn">
+      <button onClick={completeHandler} className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
       <button onClick={deleteHandler} className="trash-btn">
