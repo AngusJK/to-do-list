dc97040d284e44102a0a1758c3768511c1b5eca5
Angus Kellett <anguskellett@hotmail.com>
2021-05-31 21:23:46 +0000

    creates filteredTotos array and switch statement to fill array based on status on each todo in todos array

--- src/App.js
+++ src/App.js
@@ -8,6 +8,20 @@ function App() {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  const filterHandler = () => {
+    switch(status) {
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true))
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed !== true))
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  }
   return (
     <div className="App">
       <header>
