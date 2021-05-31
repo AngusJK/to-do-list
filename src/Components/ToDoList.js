3b99e88116378758659807beb0155f9a3f6236dd
Angus Kellett <anguskellett@hotmail.com>
2021-05-31 21:38:07 +0000

    imports filtered todos and iterates over them to display filtered results

--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ToDo from './ToDo'
 
-const ToDoList = ({ todos, setTodos }) => {
+const ToDoList = ({ todos, setTodos, filteredTodos }) => {
   return (
     <div className="todo-container">
       <ul className="todo-list">
-        {todos.map((todo) => (
+        {filteredTodos.map((todo) => (
           <ToDo 
             key={todo.id} 
             text={todo.text}
